1bafcbfd93b69703107d97eba581f57fedfa24c0
StathKar12 <87133322+StathKar12@users.noreply.github.com>
2023-04-30 03:59:15 +0300

    Delete Ex_4 directory

--- Ex_4/Ex_4.h
+++
@@ -1,8 +0,0 @@
-#include "Ex_4_Types.h"
-
-void addL(PList current,int item);
-PList Create();
-void InsertLast(PList begin,int item);
-void Print(PList begin);
-PList Unite_L(PList List_L,PList ListB);
-PList assort_L(PList List_L,PList List_E);
